247f0c296422e2fe706e10b21636000498ccf076
carlos <twincarlos98@gmail.com>
2024-08-13 10:32:08 -0400

    now im really going to start working on zustand

--- src/app/components/TournamentList/TournamentList.js
+++ src/app/components/TournamentList/TournamentList.js
@@ -1,42 +1,10 @@
-"use client";
 import "./TournamentList.css";
-import { useEffect, useState } from "react";
-import Header from "../Header/Header";
-import Window from "../Window/Window";
 import TournamentCard from "../TournamentCard/TournamentCard";
-import { CreateTournament } from "../Forms/CreateTournament";
 
-export default function TournamentList() {
-    const [tournaments, setTournaments] = useState([]);
-    const [showWindow, setShowWindow] = useState(false);
-
-    useEffect(() => {
-        async function getTournaments() {
-            const res = await fetch("/api/get-tournaments", {
-                headers: {
-                    'Cache-Control': 'no-cache',
-                    'Pragma': 'no-cache',
-                    'Expires': '0'
-                }
-            });
-            const data = await res.json();
-            setTournaments(data);
-        };
-        getTournaments();
-    }, []);
-
+export default function TournamentList({ tournaments }) {
     return (
-        <section>
-            <Header>
-                <p>Tournaments</p>
-                <button onClick={() => setShowWindow(true)}>Create tournament</button>
-            </Header>
-            <div className="tournament-list">
-                { tournaments.map(tournament => <TournamentCard tournament={tournament} key={tournament.id} />) }
-            </div>
-            <Window showWindow={showWindow} setShowWindow={setShowWindow}>
-                <CreateTournament tournaments={tournaments} setTournaments={setTournaments} />
-            </Window>
+        <section className="tournament-list">
+            {tournaments.map(tournament => <TournamentCard tournament={tournament} key={tournament.id} />)}
         </section>
     );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,39 @@
+"use client";
+import Header from "./components/Header/Header";
+import Window from "./components/Window/Window";
 import TournamentList from "./components/TournamentList/TournamentList";
+import { CreateTournament } from "./components/Forms/CreateTournament";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [tournaments, setTournaments] = useState([]);
+  const [showWindow, setShowWindow] = useState(false);
+
+  useEffect(() => {
+    async function getTournaments() {
+      const res = await fetch("/api/get-tournaments", {
+        headers: {
+          'Cache-Control': 'no-cache',
+          'Pragma': 'no-cache',
+          'Expires': '0'
+        }
+      });
+      const data = await res.json();
+      setTournaments(data);
+    };
+    getTournaments();
+  }, []);
+
   return (
     <main>
-      <TournamentList />
+      <Header>
+        <p>Tournaments</p>
+        <button onClick={() => setShowWindow(true)}>Create tournament</button>
+      </Header>
+      <TournamentList tournaments={tournaments} />
+      <Window showWindow={showWindow} setShowWindow={setShowWindow}>
+        <CreateTournament tournaments={tournaments} setTournaments={setTournaments} />
+      </Window>
     </main>
   );
 };
