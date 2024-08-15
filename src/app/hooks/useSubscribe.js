becac66554a683b8f9f2d211832ab26f2a35a2cd
carlos <twincarlos98@gmail.com>
2024-08-14 21:35:24 -0400

    added pusher instance

---
+++ src/app/hooks/useSubscribe.js
@@ -0,0 +1,11 @@
+"use client";
+import { useEffect } from "react";
+import { PusherClient } from "../../../pusher";
+
+export function useSubscribe(channelName, eventName, eventFunction) {
+    useEffect(() => {
+        var channel = PusherClient.subscribe(channelName);
+        channel.bind(eventName, eventFunction);
+        return () => PusherClient.unsubscribe(channelName);
+    }, [channelName, eventName, eventFunction]);
+};
