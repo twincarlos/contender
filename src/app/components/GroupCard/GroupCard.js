a156f9e378988a1b461dac10f8ef12ebade4def8
carlos <twincarlos98@gmail.com>
2024-08-19 16:00:51 -0400

    check point

--- src/app/components/GroupCard/GroupCard.js
+++ src/app/components/GroupCard/GroupCard.js
@@ -1,18 +1,22 @@
+"use client";
 import "./GroupCard.css";
 import MatchList from "../MatchList/MatchList";
 import GroupStandings from "../GroupStandings/GroupStandings";
 import { gmsStore, gpsStore } from "@/app/store/store";
+import { useState } from "react";
 
 export default function GroupCard({ eg }) {
     const gps = gpsStore(state => state.gps[eg.id]);
     const gms = gmsStore(state => state.gms[eg.id]);
+    const [showMatchList, setShowMatchList] = useState(false);
     return (
         <div className="group-card card">
             <div className="group-header card-header">
                 <p>Group {eg.number}</p>
             </div>
             <GroupStandings gps={Object.values(gps)} />
-            <MatchList ms={Object.values(gms)} />
+            <button onClick={() => setShowMatchList(!showMatchList)}>Matches</button>
+            {showMatchList && <MatchList ms={Object.values(gms)} />}
         </div>
     );
 };
