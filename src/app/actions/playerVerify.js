821d89cc74b04d26361908ef5d14b711972b2e27
carlos <twincarlos98@gmail.com>
2024-09-06 00:39:38 -0400

    more progress

--- src/app/actions/playerVerify.js
+++ src/app/actions/playerVerify.js
@@ -10,45 +10,50 @@ export async function playerVerify({ egId, mId, mpId }) {
     if (mp[0].verified && otherMp[0].verified) {
         const m = await db.update(ms).set({ status: "finished" }).where(eq(ms.id, mId)).returning();
 
-        const allGms = await db.query.gms.findMany({
-            with: {
-                m: {
-                    with: {
-                        mps: {
-                            with: {
-                                ep: {
-                                    with: {
-                                        tp: true
+        if (egId) {
+            const allGms = await db.query.gms.findMany({
+                with: {
+                    m: {
+                        with: {
+                            mps: {
+                                with: {
+                                    ep: {
+                                        with: {
+                                            tp: true
+                                        }
                                     }
                                 }
                             }
                         }
                     }
-                }
-            },
-            where: eq(gms.eventGroupId, egId)
-        });
-        const standings = calculateStandings(allGms.map(gm => gm.m).filter(m => m.status === "finished"));
-        const updatedGps = [];
-        let position = 1;
-
-        for (const gp of standings) {
-            const updatedGp = await db.update(gps).set({ position }).where(and(eq(gps.eventPlayerId, gp.eventPlayerId), eq(gps.eventGroupId, egId))).returning();
-            updatedGps.push(updatedGp[0]);
-            position++;
-        };
-
-        const notFinishedMs = await db.select({ count: count() })
-            .from(ms)
-            .innerJoin(gms, eq(ms.id, gms.matchId))
-            .where(and(eq(gms.eventGroupId, egId), not(eq(ms.status, "finished"))));
-
-        if (notFinishedMs[0].count === 0) {
-            const eg = await db.update(egs).set({ status: "finished" }).where(eq(egs.id, egId)).returning();
-            return { mp: mp[0], m: m[0], eg: eg[0], gps: updatedGps }; // if group is done
+                },
+                where: eq(gms.eventGroupId, egId)
+            });
+            const standings = calculateStandings(allGms.map(gm => gm.m).filter(m => m.status === "finished"));
+            const updatedGps = [];
+            let position = 1;
+    
+            for (const gp of standings) {
+                const updatedGp = await db.update(gps).set({ position }).where(and(eq(gps.eventPlayerId, gp.eventPlayerId), eq(gps.eventGroupId, egId))).returning();
+                updatedGps.push(updatedGp[0]);
+                position++;
+            };
+    
+            const notFinishedMs = await db.select({ count: count() })
+                .from(ms)
+                .innerJoin(gms, eq(ms.id, gms.matchId))
+                .where(and(eq(gms.eventGroupId, egId), not(eq(ms.status, "finished"))));
+    
+            if (notFinishedMs[0].count === 0) {
+                const eg = await db.update(egs).set({ status: "finished" }).where(eq(egs.id, egId)).returning();
+                return { mp: mp[0], m: m[0], eg: eg[0], gps: updatedGps }; // if group is done
+            } else {
+                return { mp: mp[0], m: m[0], gps: updatedGps }; // if match is done
+            };
         } else {
-            return { mp: mp[0], m: m[0], gps: updatedGps }; // if match is done
+            return { mp: mp[0] }; // update player
         };
+        
     } else {
         return { mp: mp[0] }; // update player
     };
--- src/app/components/PlayerButton/PlayerButton.js
+++ src/app/components/PlayerButton/PlayerButton.js
@@ -12,15 +12,18 @@ export default function PlayerButton({ egId, m, mp }) {
         const res = await playerCheckIn({ mId: m.id, mpId: mp.id});
         setPlayerCheckIn({ m: res.m, mp: res.mp });
     };
+
     async function handlePlayerVerify() {
         const res = await playerVerify({ egId, mId: m.id, mpId: mp.id });
         setPlayerVerify({ m: res.m, mp: res.mp });
         if (res.eg) setEgStatus(res.eg);
         if (res.gps) setGpPositions(res.gps);
     };
+
     const playerButton = {
         "ready": <button disabled={mp.checkedIn} onClick={handlePlayerCheckIn}>check in</button>,
         "pending": <button disabled={mp.verified} onClick={handlePlayerVerify}>verify</button>,
     };
+    
     return (m.status in playerButton) ? playerButton[m.status] : null;
 };
