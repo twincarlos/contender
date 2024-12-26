f822f06453e96f7025523c2acd6752e41fde73a4
carlos rodriguez <88858893+twincarlos@users.noreply.github.com>
2024-12-26 21:52:18 +0000

    fixed adding tournament bug

--- app/actions/tournaments.jsx
+++ app/actions/tournaments.jsx
@@ -4,5 +4,5 @@ import { tournamentsTable } from "@/drizzle/schema";
 import { formDataEntries } from "./utils";
 
 export async function createTournament(initialState, formData) {
-  return await db.insert(tournamentsTable).values(formDataEntries(formData)).returning();
+  return await db.insert(tournamentsTable).values(formDataEntries(formData)).returning()[0];
 };
